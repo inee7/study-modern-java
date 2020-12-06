13450c16e4cbca3f287836bdc97307d29b8680b8
inee7 <jongin012@gmail.com>
2020-12-06 22:26:40 +0900

    함수 유틸 메소드

--- src/main/java/modernjavainaction/chap03/Sorting.java
+++ src/main/java/modernjavainaction/chap03/Sorting.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Sorting {
 
@@ -57,6 +59,15 @@ public class Sorting {
             .reversed() // 역순
             .thenComparing(Apple::getColor) //같으면 이걸로 비교
     );
+
+    Predicate<Apple> redApple = apple -> apple.getColor().equals(Color.RED);
+    Predicate<Apple> notRedApple = redApple.negate(); // 반전 가능
+    Predicate<Apple> heavy = apple -> apple.getWeight() > 150; // 반전 가능
+    Predicate<Apple> andPre = heavy.and(notRedApple); // predicate 조합가능
+
+    Function<Apple, Integer> f = Apple::getWeight;
+    Function<Apple, Byte> appleByteFunction = f.andThen(Integer::byteValue);
+
   }
 
   static class AppleComparator implements Comparator<Apple> {
